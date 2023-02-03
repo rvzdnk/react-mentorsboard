f48fb0efd4d191a2a0a2b65fff55a9ecaf22c689
rvzdnk <rafal93zdunek@gmail.com>
2023-02-03 20:52:34 +0100

    add registration

--- src/components/Buttons/ShowAdvertsBtn/ShowAdvertsBtn.jsx
+++ src/components/Buttons/ShowAdvertsBtn/ShowAdvertsBtn.jsx
@@ -1,13 +1,23 @@
-// import { useAllAdvertsMutation } from "../../../redux/slices/mentorsboardApi";
+import { useGetAllAdvertsQuery } from "../../../redux/slices/mentorsboardApi";
 import { ShowMentors } from "./ShowAdvertsBtn.styled";
+import Loader from '../Loader/Loader';
 
 const ShowAdvertsBtn = () => {
 
-    // const [ allAdverts ] = useAllAdvertsMutation;
+    const {
+        data: adverts = [],
+        isLoading,
+        isSuccess,
+        isError,
+        error,
+        } = useGetAllAdvertsQuery();
 
     return (
+        <>
+        
+        </>
         <ShowMentors>Show Mentors</ShowMentors>
-    ) 
+    )
 }
 
 export default ShowAdvertsBtn;
